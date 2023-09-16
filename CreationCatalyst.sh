5e12d1511ea0cf6da310bd9e156f7ab65c67e760
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-09-16 07:19:26 +0200

    working on env template to request values

--- CreationCatalyst.sh
+++ CreationCatalyst.sh
@@ -209,16 +209,103 @@ echo "\033[1;32mDone\033[0;39m"
 echo "Creating the environment ..."
 
 sleep 1
-ENV_CONTENT="DOMAIN_NAME=zstenger.42.fr
-CERT_=
-KEY_=
-DB_NAME=dbname
-DB_ROOT=dbpass
-DB_USER=wpuser
-DB_PASS=wppass"
+ENV_TEMPLATE="
+echo \"# mariadb\" > .env
+while true; do
+    read -p \"\033[1;31mEnter the database root password:\033[0;39m \" DB_NAME
+    if [ -n \"\$DB_NAME\" ]; then
+        echo \"DB_NAME=\$DB_NAME\" >> .env
+        break
+    else
+        echo \"Input cannot be empty. Please try again.\"
+    fi
+done
 
-echo "$ENV_CONTENT" > src/.env
+while true; do
+    read -p \"\033[1;31mEnter the database user:\033[0;39m \" DB_ROOT
+    if [ -n \"\$DB_ROOT\" ]; then
+        echo \"DB_ROOT=\$DB_ROOT\" >> .env
+        break
+    else
+        echo \"Input cannot be empty. Please try again.\"
+    fi
+done
 
+while true; do
+    read -p \"\033[1;31mEnter the database:\033[0;39m \" DB_USER
+    if [ -n \"\$DB_USER\" ]; then
+        echo \"DB_USER=\$DB_USER\" >> .env
+        break
+    else
+        echo \"Input cannot be empty. Please try again.\"
+    fi
+done
+
+while true; do
+    read -p \"\033[1;31mEnter the database user password:\033[0;39m \" DB_PASS
+    if [ -n \"\$DB_PASS\" ]; then
+        echo \"DB_PASS=\$DB_PASS\" >> .env
+        break
+    else
+        echo \"Input cannot be empty. Please try again.\"
+    fi
+done
+
+echo \"# wordpress\" >> .env
+
+while true; do
+    read -p \"\033[1;31mEnter the WordPress admin:\033[0;39m \" WP_ADMIN
+    if [ -n \"\$WP_ADMIN\" ]; then
+        echo \"WP_ADMIN=\$WP_ADMIN\" >> .env
+        break
+    else
+        echo \"Input cannot be empty. Please try again.\"
+    fi
+done
+
+while true; do
+    read -p \"\033[1;31mEnter the WordPress admin password:\033[0;39m \" WP_ADMIN_PW
+    if [ -n \"\$WP_ADMIN_PW\" ]; then
+        echo \"WP_ADMIN_PW=\$WP_ADMIN_PW\" >> .env
+        break
+    else
+        echo \"Input cannot be empty. Please try again.\"
+    fi
+done
+
+while true; do
+    read -p \"\033[1;31mEnter the WordPress admin email:\033[0;39m \" WP_ADMIN_EMAIL
+    if [ -n \"\$WP_ADMIN_EMAIL\" ]; then
+        echo \"WP_ADMIN_EMAIL=\$WP_ADMIN_EMAIL\" >> .env
+        break
+    else
+        echo \"Input cannot be empty. Please try again.\"
+    fi
+done
+
+echo \"# nginx\" >> .env
+
+while true; do
+    read -p \"\033[1;31mEnter the nginx domain:\033[0;39m \" NGINX_DOMAIN
+    if [ -n \"\$NGINX_DOMAIN\" ]; then
+        echo \"NGINX_DOMAIN=\$NGINX_DOMAIN\" >> .env
+        break
+    else
+        echo \"Input cannot be empty. Please try again.\"
+    fi
+done
+"
+
+echo "$ENV_TEMPLATE" > template.sh
+
+echo "\033[1;32mDone\033[0;39m"
+echo "Creating template file for .env"
+chmod +x template.sh
+echo "\033[1;32mDone\033[0;39m"
+sleep 1
+echo "Requesting input for the .env file:"
+bash template.sh
+sleep 1
 echo "\033[1;32mDone\033[0;39m"
 sleep 1
 echo "\033[1;33mCreation has been finished, ready to go in sleep\033[0;39m"
