b9364f5fed84a5cce0d778e7553d19f098c929f1
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-09-24 09:11:34 +0200

    more alias

--- CreationCatalyst.sh
+++ CreationCatalyst.sh
@@ -11,6 +11,7 @@ echo "alias up='docker-compose up -d'" >> ~/.bashrc
 echo "alias down='docker-compose down'" >> ~/.bashrc
 echo "alias connect='docker exec -it mariadb mysql -u dbuser -p'" >> ~/.bashrc
 echo "alias rootdb='docker exec -it mariadb mysql -u root -p'" >> ~/.bashrc
+echo "alias lsd='docker ps; docker images; docker volume ls'" >> ~/.bashrc
 #open bashrc
 echo "alias a='vi ~/.bashrc'" >> ~/.bashrc
 # accessing containers
